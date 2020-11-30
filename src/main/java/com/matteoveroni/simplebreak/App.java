74953b2135786bea56050e63c31a8e2bae76185f
Matteo Veroni <matver87@gmail.com>
2020-11-30 20:08:21 +0100

    first commit

---
+++ src/main/java/com/matteoveroni/simplebreak/App.java
@@ -0,0 +1,11 @@
+package com.matteoveroni.simplebreak;
+
+public final class App {
+    public static final void main(String ... args) {
+        new App().start();
+    }
+
+    public void start(){
+        System.out.println("Hello world!");
+    }
+}
