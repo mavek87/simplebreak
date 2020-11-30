5173ef1f4a447500b30aee7f788c3bb2dd476374
Matteo Veroni <matver87@gmail.com>
2020-11-30 20:26:10 +0100

    add simple hello world for JavaFX

--- src/main/java/com/matteoveroni/simplebreak/App.java
+++ src/main/java/com/matteoveroni/simplebreak/App.java
@@ -1,11 +1,26 @@
 package com.matteoveroni.simplebreak;
 
-public final class App {
-    public static final void main(String ... args) {
-        new App().start();
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public final class App extends Application {
+
+    public static final void main(String... args) {
+        launch(args);
     }
 
-    public void start(){
-        System.out.println("Hello world!");
+    @Override
+    public void start(Stage stage) throws Exception {
+        BorderPane root = new BorderPane();
+        root.setCenter(new Button("Hello world"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setWidth(800);
+        stage.setHeight(600);
+        stage.setTitle("SimpleBreak");
+        stage.show();
     }
 }
